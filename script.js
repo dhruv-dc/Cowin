222bfbd011c1921d81590ce194d2631453fa200a
dhruv-dc <53862511+dhruv-dc@users.noreply.github.com>
2021-05-13 01:07:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,94 @@
+const url =
+  'https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=777&date=13-05-2021'
+
+// const sendGetRequest = async () => {
+//   try {
+//     const resp = await axios.get(url)
+//     const data = resp.data.centers
+
+//     console.log(data)
+//     for (var i = 0; i < data.length; i++) {
+//       for (var j = 0; j < data[i].sessions.length; j++) {
+//         if (data[i].sessions[j].min_age_limit == 18) {
+//           console.log(data[i].sessions[j].available_capacity)
+//           console.log(data[i].sessions[j].min_age_limit)
+//         }
+//       }
+//     }
+//     setTimeout('location.reload();', 1500000)
+//   } catch (err) {
+//     console.error(err)
+//   }
+// }
+function submitHandler(e) {
+  // e.preventDefault()
+  let name = document.querySelector('.name').value
+  let show = function () {
+    const noti = new Notification(`Hey ${name}`, {
+      body: 'I will notify you when seat is available',
+    })
+  }
+  show()
+}
+
+function showNotification(name, capacity) {
+  const notification = new Notification('Seat is available', {
+    body: `Hey mate, check asap ${capacity} seats are available at ${name}`,
+  })
+}
+
+function getCenters() {
+  console.log(Notification.permission)
+  if (Notification.permission === 'granted') {
+    console.log('granted')
+    // showNotification()
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission().then((permission) => {
+      if (permission === 'granted') {
+        showNotification()
+      }
+    })
+  }
+  try {
+    axios
+      .get(url)
+      .then(function (resp) {
+        const data = resp.data.centers
+        var flag = 1
+        console.log(data)
+        for (var i = 0; i < data.length; i++) {
+          for (var j = 0; j < data[i].sessions.length; j++) {
+            if (
+              data[i].sessions[j].min_age_limit == 18 &&
+              data[i].sessions[j].available_capacity > 0
+            ) {
+              showNotification(
+                data[i].name,
+                data[i].sessions[j].available_capacity
+              )
+              alert(
+                `${data[i].sessions[j].available_capacity} of seats is available`
+              )
+              console.log(data[i].name + ' name')
+              console.log(
+                data[i].sessions[j].available_capacity + ' available capacity'
+              )
+              console.log(data[i].sessions[j].min_age_limit + ' age')
+              flag = 0
+            }
+          }
+        }
+        if (flag) {
+          setTimeout('location.reload();', 150000)
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// sendGetRequest()
+getCenters()
